e73c9403c736302a91edb905f23f80530bdfdd04
jagadw <jagadhitawicaksana@gmail.com>
2025-05-15 12:46:37 +0800

    feat: add layout for ViewPoster

--- resources/views/layouts/view.blade.php
+++ resources/views/layouts/view.blade.php
@@ -6,7 +6,10 @@
     <title>e-Poster</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/css/index.css">
 </head>
+<script src="https://cdn.jsdelivr.net/npm/simple-keyboard@latest/build/index.js"></script>
+
 <body class="bg-gray-100 p-4">
     <div class="container mx-auto">
         @yield('content')
