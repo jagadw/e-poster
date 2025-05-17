7f2c24ba101e0ccbbc2e82747dc851dffd3cdf08
jagadw <jagadhitawicaksana@gmail.com>
2025-05-17 09:09:47 +0800

    feat: add file type:  docx & ppt

--- app/Http/Controllers/AdminPosterController.php
+++ app/Http/Controllers/AdminPosterController.php
@@ -61,7 +61,7 @@ class AdminPosterController extends Controller
             'name' => 'required',
             'title' => 'required',
             'affiliate' => 'nullable',
-            'file' => 'nullable|file|mimes:jpg,jpeg,png,pdf|max:10240',
+            'file' => 'nullable|file|mimes:jpg,jpeg,png,pdf,docx,ppt|max:10240',
         ]);
     
         $data = $request->only('name', 'title', 'affiliate');
