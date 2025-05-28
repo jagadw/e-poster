a708ed5a386c26b4cc65ee06b3ea2cee7593aa91
jagadw <jagadhitawicaksana@gmail.com>
2025-05-28 22:17:06 +0800

    fix: fix GuestPresentation

--- app/Http/Controllers/GuestPresentationController.php
+++ app/Http/Controllers/GuestPresentationController.php
@@ -5,28 +5,42 @@ namespace App\Http\Controllers;
 use App\Models\PodiumPresentation;
 use App\Models\Poster;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GuestPresentationController extends Controller
 {
 
     public function index(Request $request)
     {
-        $query = PodiumPresentation::query();
+        $query = DB::table('podium_presentations')
+        ->join('posters', 'podium_presentations.code', '=', 'posters.code')
+        ->select(
+            'podium_presentations.id',
+            'podium_presentations.code',
+            'posters.name',
+            'posters.title',
+            'podium_presentations.date',
+            'podium_presentations.time_start',
+            'podium_presentations.time_end',
+            'podium_presentations.room'
+        );
 
-        if ($request->filled('code')) {
-            $query->where('code', 'like', '%' . $request->code . '%');
-        }
+    if ($request->filled('code')) {
+        $query->where('podium_presentations.code', 'like', '%' . $request->code . '%');
+    }
 
-        if ($request->filled('title')) {
-            $query->where('title', 'like', '%' . $request->title . '%');
-        }
+    if ($request->filled('title')) {
+        $query->where('posters.title', 'like', '%' . $request->title . '%');
+    }
 
-        if ($request->filled('author')) {
-            $query->where('name', 'like', '%' . $request->author . '%');
-        }
+    if ($request->filled('author')) {
+        $query->where('posters.name', 'like', '%' . $request->author . '%');
+    }
 
-        $presentations = $query->paginate(10)->withQueryString();
-        return view('GuestPresentation.index', compact('presentations'));
+    $presentations = $query->paginate(10)->withQueryString();
+
+    return view('GuestPresentation.index', compact('presentations'));
+
     }
 
 }
