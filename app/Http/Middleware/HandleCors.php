927cdb159e14c23128d372f15f3f819411fdabcd
jagadw <jagadhitawicaksana@gmail.com>
2025-05-30 07:33:25 +0800

    feat: add HandleCors

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // $response = $next($request);
+        // $response->headers->set('Access-Control-Allow-Origin', '*');
+        // return $response;
+        return $next($request);
+    }
+}
